e90c16858318b6591489ada0068b541c1406dbef
Kanojah <kanoacleveland@Kanojahs-MacBook-Pro.local>
2017-03-13 18:27:44 -1000

    I'm really horrible at remembering to commit my code, I'm sorry again. here's what I got so far

--- functions.js
+++ functions.js
@@ -1,63 +1,42 @@
-/**
- * Converts a number a string.
- * @param {number} n
- * @return {string} the number as a string
- */
+var numberToString = function(x){
+  return "" + x + "";
+};
 
+var increase = function(x){
+  x += 1;
+  return x;
+};
 
-/**
- * Adds one to a given number.
- * @param {number} n
- * @return {number}
- */
+var decrease = function(x){
+  x -= 1;
+  return x;
+};
 
+var add = function(x, y){
+  return x + y;
+};
 
-/**
- * Subtracts one from a given number.
- * @param {number} n
- * @return {number}
- */
+var subtract = function(x, y){
+  return x - y;
+};
 
+var multiply = function(x, y){
+  return x * y;
+};
 
-/**
- * Adds two numbers.
- * @param {number} x
- * @param {number} y
- * @return {number} the sum
- */
+var divide = function(x, y){
+  return x / y;
+};
 
+var square = function(x){
+  return x * x;
+};
 
-/**
- * Subtracts the second number from the first.
- * @param {number} x
- * @param {number} y
- * @return {number} the difference
- */
-
-
-/**
- * Multiplies two numbers.
- * @param {number} x
- * @param {number} y
- * @return {number} the product
- */
-
-
-/**
- * Divides the first number by the second.
- * @param {number} x
- * @param {number} y
- * @return {number} the quotient
- */
-
-
-/**
- * Multiplies a number by itself.
- * @param {number} x, number to be squared
- * @return {number} squared
- */
-
-
+var calculate = function(add, x, y){
+  var result = x + y;
+  console.log(x + " + " + y + " = " + result);
+  return result;
+};
 /**
  * Performs a mathematical operation on two numbers.
  * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
@@ -67,83 +46,87 @@
  * @return {number} the result
  */
 
+var isGreaterThan = function(x, y){
+  if(x > y){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-/**
- * Returns true if `a` is greater than `b`.
- * @param {number} a
- * @param {number} b
- * @return {boolean} `a` is larger than `b`
- */
+var isLessThan = function(x, y){
+  if(x < y){
+    return true;
+  } else {
+    return false;
+  }
+};
 
+var areEqual = function(x, y){
+  if(x === y){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-/**
- * Returns true if `a` is less than `b`.
- * @param {number} a
- * @param {number} b
- * @return {boolean} `a` is smaller than `b`
- */
+var minimum = function(x, y){
+  if(x < y){
+    return x;
+  } else {
+    return y;
+  }
+};
 
+var maximum = function(x, y){
+  if(x > y){
+    return x;
+  } else {
+    return y;
+  }
+};
 
-/**
- * Returns true if `a` and `b` are equal.
- * @param {number} a
- * @param {number} b
- * @return {boolean} the numbers are equal
- */
+var isEven = function(n){
+  if(n % 2 === 0){
+    return true;
+  } else {
+    return false;
+  }
+};
 
+var isOdd = function(n){
+  if(n % 2 !== 0){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-/**
- * Returns the smallest value of two numbers.
- * @param {number} x
- * @param {number} y
- * @return {number} the smallest number
- */
+var letterGrade = function(x, y){
+  if(x / y >= 0.9 && x / y <= 1){
+    return "A";
+  } else if(x / y >= 0.8 && x / y <= 0.9){
+    return "B";
+  } else if(x / y >= 0.7 && x / y <= 0.8){
+    return "C";
+  } else if(x / y >= 0.6 && x / y <= 0.7){
+    return "D";
+  } else if(x / y >= 0 && x / y <= 0.6){
+    return "F";
+  }
+};
 
+var incrementReviews = function(r){
+  if(r.reviews === undefined){
+    r.reviews = 0;
+  }
+    r.reviews++;
+    return r;
+};
 
-/**
- * Returns the largest value of two numbers.
- * @param {number} x
- * @param {number} y
- * @return {number} the largest number
- */
-
-
-/**
- * Returns true if `n` is even.
- * @param {number} n
- * @return {boolean} the number is even
- */
-
-
-/**
- * Returns true if `n` is odd.
- * @param {number} n
- * @return {boolean} the number is odd
- */
-
-
-/**
- * Returns a letter grade.
- * "A": 90-100%
- * "B": 80-89%
- * "C": 70-79%
- * "D": 60-69%
- * "F": 0-59%
- * @param {number} score
- * @param {number} total maximum possible score
- * @return {string} the score represented as a letter grade
- */
-
-
-/**
- * Checks if a `restaurant` object has a `reviews` property.
- * If it does, increase the property's `reviews` value by 1.
- * If it does not, set the `reviews` value to 1.
- * @param {object} restaurant   represents a restaurant object
- * @return {object} restaurant
- */
-
-
+var combine = function(a, b){
+  return "a " + "b";
+};
 /**
  * Joins two strings with a space.
  * @param {string} word1
@@ -151,12 +134,9 @@
  * @return {string} joined the words joined with a space
  */
 
-
-/**
- * Returns a circle object with the properties `circumference` and `area`.
- * Use Math.PI for the value π.
- * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI)
- * @param {number} radius
- * @return {object} circle
- */
-
+var createCircle = function(r){
+  var circle = {};
+  circle.circumference = (2 * Math.PI * r);
+  circle.area = (Math.PI * r * r);
+  return circle;
+};
